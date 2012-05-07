dd9b6b3901fc0bfb0faea540ffd1f23a30e6ac19
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-05-07 12:26:51 +0400

    categorization strategies

--- doppler/shift/catalog/models.py
+++ doppler/shift/catalog/models.py
@@ -8,23 +8,12 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from mptt.models import MPTTModel
 from .managers import EnabledTreeManager, EnabledRootManager
+from django.conf import settings
 
-class Product(models.Model):
-    """
-    Basic catalog entity - a product
-    """
-    class Meta:
-        verbose_name = _('product')
-        verbose_name_plural = _('products')
-        ordering = ['name']
-    name = models.CharField(max_length=255, verbose_name=_('name'))
-    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
-    enabled = models.BooleanField(default=True, verbose_name=_('enabled'))
-    created = models.DateTimeField(auto_now_add = True, verbose_name = _('created'))
-    modified = models.DateTimeField(auto_now = True, verbose_name = _('modified'))
-
-    def __unicode__(self):
-        return self.name
+try:
+    MULTIPLE_CATEGORIES = settings.DOPPLER_SHIFT_CATALOG_PRODUCT_MULTIPLE_CATEGORIES
+except AttributeError:
+    MULTIPLE_CATEGORIES = False
 
 class Category(MPTTModel):
     """
@@ -44,6 +33,30 @@ class Category(MPTTModel):
 
     enabled_tree = EnabledTreeManager()
     enabled_root = EnabledRootManager()
+
+    def __unicode__(self):
+        return self.name
+
+class Product(models.Model):
+    """
+    Basic catalog entity - a product
+    """
+    class Meta:
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+        ordering = ['name']
+    name = models.CharField(max_length=255, verbose_name=_('name'))
+    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
+    enabled = models.BooleanField(default=True, verbose_name=_('enabled'))
+    created = models.DateTimeField(auto_now_add = True, verbose_name = _('created'))
+    modified = models.DateTimeField(auto_now = True, verbose_name = _('modified'))
+    # categorization may differ depending on current store
+    if MULTIPLE_CATEGORIES:
+        categories = models.ManyToManyField(to=Category, blank=True, null=True,
+            verbose_name=_('categories'), related_name='products')
+    else:
+        category = models.ForeignKey(to=Category, blank=True, null=True,
+            verbose_name=_('category'), related_name='products')
 
     def __unicode__(self):
         return self.name
