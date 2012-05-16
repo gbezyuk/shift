ee5e279dbfa3ec25a246e3a6ee6381e42ff61c6e
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-05-16 09:18:15 +0400

    cart model updated

--- doppler/shift/checkout/models.py
+++ doppler/shift/checkout/models.py
@@ -5,6 +5,7 @@ Module: Checkout
 Part: Models implementation
 """
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 from django.db import models
 from doppler.shift.catalog.models import Product
 import random
@@ -16,17 +17,24 @@ class Cart(models.Model):
     created = models.DateTimeField(auto_now_add = True, verbose_name = _('created'))
     modified = models.DateTimeField(auto_now = True, verbose_name = _('modified'))
     key = models.CharField(max_length = 50, verbose_name = _('session key'))
+    user = models.ForeignKey(to=User, verbose_name=_('user'), blank=True, null=True)
 
     CART_ID_SESSION_KEY = 'cart_id' #TODO: move to settings configuration file
     CART_ID_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
 
     @classmethod
     def _generate_cart_id(cls):
+        """
+        Private method for unique cart id generation
+        """
+        # setting generation params
         cart_id = ''
         characters = cls.CART_ID_CHARACTERS
         cart_id_length = 50
+        # generating key
         for y in range(cart_id_length):
             cart_id += characters[random.randint(0, len(characters)-1)]
+        # preventing even extremely low-possible random match
         try:
             Cart.objects.get(key=cart_id)
             cart_id = cls._generate_cart_id()
@@ -35,25 +43,47 @@ class Cart(models.Model):
 
     @classmethod
     def get_session_stored_cart_id(cls, request):
+        """
+        Retrieve session-stored cart-id
+        """
         if not request.session.get(cls.CART_ID_SESSION_KEY, False):
             request.session[cls.CART_ID_SESSION_KEY] = cls._generate_cart_id()
         return request.session[cls.CART_ID_SESSION_KEY]
 
     @classmethod
-    def get_cart(cls, request):
+    def get_cart(cls, request, create_if_not_exist=False):
+        """
+        Get cart model basing on provided request
+        """
         key = cls.get_session_stored_cart_id(request)
         try:
             return Cart.objects.get(key=key)
         except Cart.DoesNotExist:
-            return None
+            if create_if_not_exist and key:
+                return Cart(key=key, user=request.user if request.user.authenticated() else None)
+            else:
+                return None
 
     @classmethod
     def get_cart_items(cls, request):
+        """
+        Get cart items basing on provided request
+        """
         cart = cls.get_cart(request)
         if cart:
             return cart.cart_items.all()
         else:
             return []
+
+    def insert_item(self, product, quantity, price):
+        """
+        Insert item to the cart instance
+        """
+        try:
+            self.cart_items.get(product=product, price=price).augment_quantity(quantity)
+        except CartItem.DoesNotExist:
+            CartItem(product=product, price=price, quantity=quantity, cart=self).save()
+
 
 class CartItem(models.Model):
     """
@@ -66,7 +96,7 @@ class CartItem(models.Model):
 
     cart = models.ForeignKey(to=Cart, verbose_name=_('cart'), related_name='cart_items')
     product = models.ForeignKey(to=Product, verbose_name=_('product'))
-    price = models.DecimalField(verbose_name=_('price'), max_digits=10, decimal_places=3)
+    price = models.PositiveIntegerField(verbose_name=_('price'), default=0)
     quantity = models.IntegerField(default = 1, verbose_name = _('quantity'))
     created = models.DateTimeField(auto_now_add = True, verbose_name = _('created'))
     modified = models.DateTimeField(auto_now = True, verbose_name = _('modified'))
