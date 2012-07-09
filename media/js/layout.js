257556d48a9f9a80f7b12bb73b48b5f3f19373b2
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-07-09 16:46:23 +0400

    catalog widget sidebar layout fixed

--- media/js/layout.js
+++ media/js/layout.js
@@ -1,12 +1,1 @@
-function fetch_sidebar_height() {
-    var $sidebar = $('.catalog_widget');
-    var $rest = $sidebar.parent();//$('.rest');
-    var rest_height = $rest.height();
-    $sidebar.height(rest_height);
-}
-
-$(function () {
-    $(window).resize(fetch_sidebar_height);
-    fetch_sidebar_height();
-    setInterval(fetch_sidebar_height, 500);
-});
+// nothing here now
