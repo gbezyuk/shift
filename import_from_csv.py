d1c4b83ca4e967d80f75d74e60d98bc5beeb8398
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-11-27 23:39:53 +0400

    new import

--- import_from_csv.py
+++ import_from_csv.py
@@ -1,16 +1,9 @@
-#1. drop all existing shipments
-#2. drop all existing sizes
-#3. create new sizes
-#4. for each object put existing shipments
-#5. ???
-#6. PROFIT
-
 def import_from_csv(filename):
     f = open(filename)
     parsed = []
     for s in f:
         parsed.append(s.split(','))
-    objects = [{'category': p[0], 'name': p[1], 'slug': p[2], 'sizes': p[3:-1], 'price': p[-1]} for p in parsed[3:]]
+    objects = [{'category': p[0], 'name': p[1], 'slug': p[2], 'sizes': p[3:-2], 'price': int("0"+(p[-2]+p[-1]).replace('"', ''))/100} for p in parsed[3:]]
 
     i = 0
     sizes = []
@@ -19,3 +12,32 @@ def import_from_csv(filename):
         i += 1
 
     return sizes, objects
+
+def import_sizes(sizes):
+    from doppler.shift.catalog.models import Size
+    for s in sizes:
+        Size(title=s[-1]).save()
+
+def import_products(products, mens_category, womens_category, childrens_category, quantity=100):
+    from doppler.shift.catalog.models import Product, Shipment
+    for p in products:
+        if p['category'] == '\xd0\xbc\xd1\x83\xd0\xb6':
+            c = mens_category
+        elif p['category'] == '\xd0\xb6\xd0\xb5\xd0\xbd' :
+            c = womens_category
+        c = childrens_category
+        pd = Product(category=c, name=p['name'], name_en=p['name'], name_ru=p['name'], base_price=p['price'], slug=p['slug'])
+        pd.save()
+        for s in range(len(p['sizes'])): #bug here
+            Shipment(product=pd, size_id=s+1, remainder=quantity, enabled=True).save()
+
+"""
+# %cpaste it via ipython django shell
+from import_from_csv import *
+from doppler.shift.catalog.models import *
+Product.objects.all().delete()
+sizes, products = import_from_csv('catalog.csv')
+#import_sizes(sizes)
+m,f,c = Category.objects.all()
+import_products(products, m, f, c)
+"""
